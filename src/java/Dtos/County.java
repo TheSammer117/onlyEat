c2be30c0549fe091827a5d7b5c84085da0585565
82509 <82509@DESKTOP-LFCKJ4G>
2019-02-11 23:19:24 +0000

    county dto

---
+++ src/java/Dtos/County.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dtos;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author 82509
+ */
+public class County {
+
+    private int countyId;
+    private String name;
+
+    public County() {
+        this.countyId = 0;
+        this.name = null;
+    }
+
+    public County(int countyId, String name) {
+        this.countyId = countyId;
+        this.name = name;
+    }
+
+    public int getCountyId() {
+        return countyId;
+    }
+
+    public void setCountyId(int countyId) {
+        this.countyId = countyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.countyId;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final County other = (County) obj;
+        if (this.countyId != other.countyId) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "County{" + "countyId=" + countyId + ", name=" + name + '}';
+    }
+
+}
