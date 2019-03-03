24599506d5ce950070b9ad360cca55d8b996b666
82509 <82509@DESKTOP-LFCKJ4G>
2019-03-03 19:05:35 +0000

    delete

--- src/java/Commands/DeleteFoodCommand.java
+++ src/java/Commands/DeleteFoodCommand.java
@@ -5,8 +5,10 @@
  */
 package Commands;
 
+import Daos.FoodDao;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -19,7 +21,42 @@ public class DeleteFoodCommand implements Command {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          String forwardToJsp = "";
+        int restaurantId = Integer.parseInt(request.getParameter("restaurantId"));
+        if (restaurantId != 0) {
+            int foodId = 0;
+            try {
+            
+                foodId = Integer.parseInt(request.getParameter("foodId"));
+            } catch (NumberFormatException e) {
+                forwardToJsp = "error.jsp";
+                HttpSession session = request.getSession();
+                session.setAttribute("errorMessage", "Notvaliddata");
+            }
+
+            if (restaurantId != -1 && foodId != -1 ) {
+                FoodDao fDao = new FoodDao("delivery");
+                int rowsAffected = 0;
+                rowsAffected= fDao.removeFood(restaurantId, foodId);
+                if (rowsAffected != -1) {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("successMessage", "foodDeletedSuccessfully");
+                    forwardToJsp = "restaurantIndex.jsp";
+                } else {
+                    forwardToJsp = "error.jsp";
+                    HttpSession session = request.getSession();
+                    session.setAttribute("errorMessage", "somethingWentWrong");
+                }
+            } else {
+                forwardToJsp = "error.jsp";
+
+                HttpSession session = request.getSession();
+
+                session.setAttribute("errorMessage", "A parameter value required for viewing the menu was missing.");
+            }
+
+        }
+        return forwardToJsp;
     }
     
 }
